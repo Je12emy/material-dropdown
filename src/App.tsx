32394cd366d39d7bda3caaf66896951921c55160
Jeremy <jeremy@je12emy.com>
2023-03-02 15:22:19 -0600

    feat: add collapse in list item

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,19 @@ import {
   ListItemButton,
   IconButton,
   Container,
-  Box,
   Paper,
   Divider,
+  ListItemIcon,
+  Collapse,
+  Box,
 } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Delete, ExpandMore, ExpandLess } from "@mui/icons-material";
 import { z } from "zod";
 
 const optionSchema = z.object({
   id: z.number(),
   label: z.string(),
+  description: z.string(),
 });
 
 type option = z.infer<typeof optionSchema>;
@@ -29,52 +32,72 @@ const options: option[] = [
   {
     id: 1,
     label: "How to deal with stress",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 2,
     label: "How deal with anxiety",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 3,
     label: "What is PTSD?",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 4,
     label: "The benefits of meditation",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 5,
     label: "What is insomnia",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 6,
     label: "Why you shouldn't self medicate",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
 
-const AssetList: React.FC<{ assets: option[] }> = ({ assets }) => (
-  <Paper sx={{ width: "100%", marginTop: "0.5rem" }}>
-    <List>
-      {assets.map((asset, i) => (
-        <>
-          <ListItem
-            disablePadding
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <Delete />
-              </IconButton>
-            }
-          >
-            <ListItemButton>
-              <ListItemText primary={asset.label} />
-            </ListItemButton>
-          </ListItem>
-          {i !== assets.length - 1 && <Divider />}
-        </>
-      ))}
-    </List>
-  </Paper>
-);
+const AssetItem: React.FC<{ asset: option }> = ({ asset }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <ListItem
+        disablePadding
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <Delete />
+          </IconButton>
+        }
+      >
+        <ListItemButton>
+          <ListItemIcon onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItemIcon>
+          <ListItemText primary={asset.label} />
+        </ListItemButton>
+      </ListItem>
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        <Box sx={{ margin: "0.75rem" }}>
+          <Typography variant="body1" gutterBottom align="left">
+            {asset.description}
+          </Typography>
+        </Box>
+      </Collapse>
+    </>
+  );
+};
+
+const AssetList: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <Paper sx={{ width: "100%", marginTop: "0.5rem" }}>
+      <List>{children}</List>
+    </Paper>
+  );
+};
 
 export default function App() {
   const [option, setOption] = React.useState<option | null>(null);
@@ -90,7 +113,7 @@ export default function App() {
   return (
     <Container>
       <Stack sx={{ width: "100%" }} direction="column" alignItems="center">
-        <Typography variant="h3" gutterBottom>
+        <Typography variant="h4" gutterBottom>
           Program Assets
         </Typography>
         <Stack
@@ -115,7 +138,14 @@ export default function App() {
           <Button onClick={onSubmit}> Add </Button>
         </Stack>
         {assets.length > 0 ? (
-          <AssetList assets={assets} />
+          <AssetList>
+            {assets.map((asset, i) => (
+              <>
+                <AssetItem asset={asset} />{" "}
+                {i !== assets.length - 1 && <Divider />}
+              </>
+            ))}
+          </AssetList>
         ) : (
           <Typography
             variant="body1"
